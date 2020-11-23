3e2f8ed35054a83a6fffcb756b91b2d2d0d12a54
Jeremias Deck <deck@pluspol-interactive.de>
2020-11-23 18:47:20 +0100

    subtask 1.2: adds table styling

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <ul v-for="employee in employees" :key="employee.id">
+  <div class="container">
+    <h1> Employees</h1>
+    <ul class="row header">
+      <li>Name</li>
+      <li>Age</li>
+      <li>Salary</li>
+    </ul>
+    <ul class="row" v-for="employee in employees" :key="employee.id">
       <li>{{employee.employee_name}}</li>
       <li>{{employee.employee_age}}</li>
       <li>{{employee.employee_salary}}</li>
@@ -29,5 +35,52 @@ export default {
 </script>
 
 <style scoped>
+
+.container {
+  width: 100%;
+}
+
+.row {
+  list-style-type: none;
+  display: grid;
+  grid-template-columns: repeat(12, [col-start] 1fr);
+  padding: 0;
+  margin: 10px 0;
+}
+
+.header {
+  font-weight: bold;
+}
+
+.row > li {
+  float: left;
+  grid-column: span 2;
+  border-bottom: 1px solid lightgrey;
+  padding: 10px 0;
+}
+
+.row > li:first-child {
+  grid-column: col-start 1 / span 3;
+}
+
+@media (max-width: 768px) {
+  .row > li:first-child {
+    grid-column: col-start 1 / span 5;
+  }
+
+  .row > li {
+    grid-column: span 3
+  }
+}
+
+@media (max-width: 480px) {
+  .row > li:first-child {
+    grid-column: col-start 1 / span 5;
+  }
+
+  .row > li{
+    grid-column: span 3;
+  }
+}
 
 </style>
