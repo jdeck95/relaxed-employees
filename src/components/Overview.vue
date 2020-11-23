d93219231ebdf2acfccb0e088474c172dc3d0f86
Jeremias Deck <deck@pluspol-interactive.de>
2020-11-23 17:48:08 +0100

    subtask 1.2: fetches employees

---
+++ src/components/Overview.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <ul v-for="employee in employees" :key="employee.id">
+      <li>{{employee.employee_name}}</li>
+      <li>{{employee.employee_age}}</li>
+      <li>{{employee.employee_salary}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Overview',
+  data: () => {
+    return {
+      employees: [],
+    };
+  },
+  async created() {
+    await this.fetchEmployees();
+  },
+  methods: {
+    async fetchEmployees() {
+      const response = await fetch('http://localhost:81/api/v1/employees');
+      this.employees = await response.json();
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
