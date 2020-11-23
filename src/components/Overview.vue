542978cbbb58367f24b603dc007f7c2d5379fdc3
Jeremias Deck <deck@pluspol-interactive.de>
2020-11-23 20:55:14 +0100

    doc: adds some comments

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -19,10 +19,20 @@ export default {
   name: 'Overview',
   data: () => {
     return {
+      /**
+       * List of employees
+       */
       employees: [],
+      /**
+       * type of sorting of the employees attributes
+       * @values empty, asc (ascending), desc (descending)
+       */
       nameSorted: '',
       ageSorted: '',
       salarySorted: '',
+      /**
+       * HTML Code of the icons
+       */
       icons: {
         asc: '&#8595;',
         desc: '&#8593;',
@@ -33,16 +43,32 @@ export default {
     await this.fetchEmployees();
   },
   methods: {
+    // fetches employees from API and assigns it to the state
+    /**
+       * Gets called after the component was created
+       * Fetches employees from API and assigns them to the state
+       */
     async fetchEmployees() {
       const response = await fetch('http://localhost:81/api/v1/employees');
       this.employees = await response.json();
+      // adds dummy-employee for testing purposes
       this.employees.push({"id":"3","employee_name":"Peter Petersen","employee_age":21,"employee_salary":10,"profile_image":""})
     },
+
+    /**
+       * Resets the states, which describes in what order the attributes are sorted; empty string stands for unsorted
+       */
     removeIcons() {
       this.nameSorted = '';
       this.ageSorted = '';
       this.salarySorted = '';
     },
+
+    /**
+       * Sorts employees on the given attribute
+       *
+       * @param {String} sort The name of the attribute
+       */
     sortEmployees(sort) {
       const attribute = `employee_${sort}`;
       const sortOrder = `${sort}Sorted`;
